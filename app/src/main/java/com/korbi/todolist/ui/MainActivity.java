cc48a72a1a5003edadbed3c691f4ac364be45141
korbi98 <korbinianmoser98@gmail.com>
2017-07-02 17:59:18 +0200

    Added navigation view to select different tasklists (e.g. shopping list, work, private etc.) (multiple list haven't been implemented yet)

--- app/src/main/java/com/korbi/todolist/ui/MainActivity.java
+++ app/src/main/java/com/korbi/todolist/ui/MainActivity.java
@@ -12,15 +12,18 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -37,8 +40,9 @@ import com.korbi.todolist.todolist.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity //TODO create own Icon
-                                                    //TODO maybe change widget to just textviews in LinearLayout instead of listview
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+//TODO create own Icon
+// TODO maybe change widget to just textviews in LinearLayout instead of listview
 {
     public static TaskDbHelper db;
     public static List<Task> taskItems;
@@ -46,6 +50,7 @@ public class MainActivity extends AppCompatActivity //TODO create own Icon
     public  ToDoListRecyclerAdapter adapter;
     public RecyclerView rv;
     private TextView emptylist;
+    private DrawerLayout drawer;
 
     public static SharedPreferences settings;
     public Snackbar undoDeleteSnack;
@@ -60,6 +65,15 @@ public class MainActivity extends AppCompatActivity //TODO create own Icon
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         db = new TaskDbHelper(this);
 
@@ -184,18 +198,19 @@ public class MainActivity extends AppCompatActivity //TODO create own Icon
     }
 
     @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.context_menu, menu);
     }
 
     @Override
@@ -358,6 +373,16 @@ public class MainActivity extends AppCompatActivity //TODO create own Icon
     {
         if(taskItems.size() == 0) emptylist.setVisibility(View.VISIBLE);
         else emptylist.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
